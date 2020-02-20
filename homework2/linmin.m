fa0095106ba435dc497f035c4b1f244aca4d02e1
Dylan Rodriquez <dylanmr@uchicago.edu>
2020-02-19 21:45:24 -0500

    finished q5

---
+++ homework2/linmin.m
@@ -0,0 +1,10 @@
+function tj = linmin(X,y,j, thetaS)
+    calcx = X(:,j);
+    if isempty(thetaS)
+        thetaS = zeros(size(X,1),1);
+    end
+    calcy = y-dot(thetaS,calcx);
+
+    % argmin 1/2 for linreg
+    tj = linreg(calcx, calcy);
+end
